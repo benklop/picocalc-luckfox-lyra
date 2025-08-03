f100b78dc67c8c7150d2ea2c231d6c677eb6754b
Hpsaturn <hpsaturn@gmail.com>
2025-08-04 01:55:52 +0200

    added missing doc and pushed missing commit

--- base/buildroot/package/x48ng/x48ng.mk
+++ base/buildroot/package/x48ng/x48ng.mk
@@ -19,13 +19,14 @@ X48NG_DEPENDENCIES = \
   lua
 
 X48NG_MAKE_ENV = \
-  HAS_X11=0 \
   WITH_X11=no \
   PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)" \
   PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig"
 
+# Build flags with explicit
+# the CC definition here fixed this issue:
+# https://github.com/gwenhael-le-moine/x48ng/issues/29
 
-# Build flags with explicit include paths
 X48NG_MAKE_OPTS = \
   WITH_X11=no \
   HAS_X11=0 \
@@ -37,21 +38,21 @@ X48NG_MAKE_OPTS = \
   PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)" \
   PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig"
 
-# Build command
 define X48NG_BUILD_CMDS
     $(X48NG_MAKE_ENV) $(MAKE) -C $(@D) $(X48NG_MAKE_OPTS)
 endef
 
-# Custom install command
+# Custom install command (some errors with original MakeFile
 define X48NG_INSTALL_TARGET_CMDS
-    # Install binary
-    $(INSTALL) -D -m 0755 $(@D)/dist/x48ng $(TARGET_DIR)/usr/bin/x48ng
-    
-    # Install icon
-    $(INSTALL) -D -m 0644 $(@D)/dist/hplogo.png $(TARGET_DIR)/usr/share/x48ng/hplogo.png
-    
+	# Install binary
+	$(INSTALL) -D -m 0755 $(@D)/dist/x48ng $(TARGET_DIR)/usr/bin/x48ng
+	
+	# Install icon
+	$(INSTALL) -D -m 0644 $(@D)/dist/hplogo.png $(TARGET_DIR)/usr/share/x48ng/hplogo.png
+	
 	cp -R $(@D)/dist/ROMs/ $(TARGET_DIR)/usr/share/x48ng/
 	$(INSTALL) -c -m 755 $(@D)/dist/setup-x48ng-home.sh $(TARGET_DIR)/usr/share/x48ng/setup-x48ng-home.sh
 	chmod 755 $(TARGET_DIR)/usr/share/x48ng/setup-x48ng-home.sh
 endef
+
 $(eval $(generic-package))
