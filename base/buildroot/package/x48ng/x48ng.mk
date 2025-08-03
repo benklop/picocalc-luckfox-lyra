5b9df5e0d7306bfc58cf5c038452fdbbca651b49
Hpsaturn <hpsaturn@gmail.com>
2025-08-03 20:44:14 +0200

    seems that this fix the issues around gnu abi mfloat flag
    https://github.com/gwenhael-le-moine/x48ng/issues/29

--- base/buildroot/package/x48ng/x48ng.mk
+++ base/buildroot/package/x48ng/x48ng.mk
@@ -14,31 +14,29 @@ HTOP_CONF_ENV = HTOP_NCURSES_CONFIG_SCRIPT=$(STAGING_DIR)/usr/bin/$(NCURSES_CONF
 
 # Dependencies from .github/workflows/c-cpp.yml
 X48NG_DEPENDENCIES = \
-    ncurses \
-    readline \
-    lua
+  ncurses \
+  readline \
+  lua
 # Build flags with explicit include paths
 X48NG_MAKE_OPTS = \
-    HAS_X11=0 \
-    HAS_SLD2=0 \
-    PREFIX=/usr \
-    CFLAGS="$(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include" \
-	CXXFLAGS="$(TARGET_CXXFLAGS)" \
-    LDFLAGS="$(TARGET_LDFLAGS) -lstdc++ -lm -L$(STAGING_DIR)/usr/lib" \
-	CROSS_COMPILE=$(TARGET_CROSS) \
-	PKG_CONF_PATH="$(PKG_CONFIG_HOST_BINARY)" \
-    PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)" \
-    PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
-
+  WITH_X11=no \
+  HAS_X11=0 \
+  PREFIX=/usr \
+  CC="$(TARGET_CC)" \
+  CFLAGS="$(TARGET_CFLAGS) -mfloat-abi=hard -I$(STAGING_DIR)/usr/include" \
+  LDFLAGS="$(TARGET_LDFLAGS) -mfloat-abi=hard" \
+  PKG_CONF_PATH="$(PKG_CONFIG_HOST_BINARY)" \
+  PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)" \
+  PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
 
 # Build command
 define X48NG_BUILD_CMDS
-    $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(X48NG_MAKE_OPTS)
+    $(X48NG_MAKE_ENV) $(MAKE) -C $(@D) $(X48NG_MAKE_OPTS)
 endef
 
 # Install command
 define X48NG_INSTALL_TARGET_CMDS
-    $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+    $(X48NG_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) $(X48NG_MAKE_OPTS) install
 endef
 
 $(eval $(generic-package))
