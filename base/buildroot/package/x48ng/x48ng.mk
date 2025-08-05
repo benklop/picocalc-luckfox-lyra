ead7e658c8559d442b28b4ff11ddd95098e2f969
Hpsaturn <hpsaturn@gmail.com>
2025-08-05 18:08:03 +0200

    fixed buffer overflow issue. Seems that missing debug flags

--- base/buildroot/package/x48ng/x48ng.mk
+++ base/buildroot/package/x48ng/x48ng.mk
@@ -29,30 +29,38 @@ X48NG_MAKE_ENV = \
 
 X48NG_MAKE_OPTS = \
   WITH_X11=no \
+  WITH_SDL2=no \
   HAS_X11=0 \
   PREFIX=/usr \
   CC="$(TARGET_CC)" \
-  CFLAGS="$(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include" \
-  LDFLAGS="$(TARGET_LDFLAGS)" \
+  CFLAGS="$(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include \
+    -g3 -ggdb3 -O0 -rdynamic \
+    -fno-stack-protector \
+    -z execstack \
+    -D_FILE_OFFSET_BITS=64 \
+    -Wno-absolute-value \
+    -Wno-sign-compare \
+    -Wno-unknown-warning-option" \
+  LDFLAGS="$(TARGET_LDFLAGS) -mfloat-abi=hard" \
   PKG_CONF_PATH="$(PKG_CONFIG_HOST_BINARY)" \
   PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)" \
   PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig"
 
 define X48NG_BUILD_CMDS
-    $(X48NG_MAKE_ENV) $(MAKE) -C $(@D) $(X48NG_MAKE_OPTS)
+  $(X48NG_MAKE_ENV) $(MAKE) -C $(@D) $(X48NG_MAKE_OPTS)
 endef
 
 # Custom install command (some errors with original MakeFile
 define X48NG_INSTALL_TARGET_CMDS
-	# Install binary
-	$(INSTALL) -D -m 0755 $(@D)/dist/x48ng $(TARGET_DIR)/usr/bin/x48ng
-	
-	# Install icon
-	$(INSTALL) -D -m 0644 $(@D)/dist/hplogo.png $(TARGET_DIR)/usr/share/x48ng/hplogo.png
-	
-	cp -R $(@D)/dist/ROMs/ $(TARGET_DIR)/usr/share/x48ng/
-	$(INSTALL) -c -m 755 $(@D)/dist/setup-x48ng-home.sh $(TARGET_DIR)/usr/share/x48ng/setup-x48ng-home.sh
-	chmod 755 $(TARGET_DIR)/usr/share/x48ng/setup-x48ng-home.sh
+  # Install binary
+  $(INSTALL) -D -m 0755 $(@D)/dist/x48ng $(TARGET_DIR)/usr/bin/x48ng
+  
+  # Install icon
+  $(INSTALL) -D -m 0644 $(@D)/dist/hplogo.png $(TARGET_DIR)/usr/share/x48ng/hplogo.png
+  
+  cp -R $(@D)/dist/ROMs/ $(TARGET_DIR)/usr/share/x48ng/
+  $(INSTALL) -c -m 755 $(@D)/dist/setup-x48ng-home.sh $(TARGET_DIR)/usr/share/x48ng/setup-x48ng-home.sh
+  chmod 755 $(TARGET_DIR)/usr/share/x48ng/setup-x48ng-home.sh
 endef
 
 $(eval $(generic-package))
