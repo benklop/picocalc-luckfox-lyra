19cf6442ff08f9eef609b2a1184159cf29cc13e8
Hpsaturn <hpsaturn@gmail.com>
2025-10-14 00:24:07 +0200

    fixed hash commit issue

--- base/buildroot/package/x48ng/x48ng.mk
+++ base/buildroot/package/x48ng/x48ng.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-X48NG_VERSION = 6a6b912e796f259231e1e42c5e3c7e51a37121e7
-X48NG_SITE = https://github.com/hpsaturn/x48ng.git
+X48NG_VERSION = 7adbd27f66e7e04ee3c7288d39469585f1d
+X48NG_SITE = https://github.com/gwenhael-le-moine/x48ng
 X48NG_SITE_METHOD = git
 X48NG_LICENSE = GPL2
 X48NG_LICENSE_FILES = COPYING
@@ -20,7 +20,7 @@ X48NG_DEPENDENCIES = \
 
 X48NG_MAKE_ENV = \
   WITH_X11=no \
-  WITH_SDL2=no \
+  WITH_SDL=no \
   PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)" \
   PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig"
 
@@ -30,7 +30,7 @@ X48NG_MAKE_ENV = \
 
 X48NG_MAKE_OPTS = \
   WITH_X11=no \
-  WITH_SDL2=no \
+  WITH_SDL=no \
   HAS_X11=0 \
   PREFIX=/usr \
   CC="$(TARGET_CC)" \
