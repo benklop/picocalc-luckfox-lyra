c9da9e407bcfe67950555d57c136191e9603730a
Hpsaturn <hpsaturn@gmail.com>
2025-11-12 00:37:56 +0100

    initial version for build GNU Chess basic SDL frontend app

--- base/buildroot/package/chessboard/chessboard.mk
+++ base/buildroot/package/chessboard/chessboard.mk
@@ -4,9 +4,13 @@
 #
 ################################################################################
 
-CHESSBOARD_VERSION = 6b461aabeb14d2e55fa891b6d955ca54d5f850c1
+CHESSBOARD_VERSION = 789bc8ce7d704f3dee3067e6f425467eb7158c3d
 CHESSBOARD_SITE= https://github.com/hpsaturn/chessboard
 CHESSBOARD_SITE_METHOD = git
+
+#CHESSBOARD_VERSION = local
+#CHESSBOARD_SITE= $(TOPDIR)/../chessboard
+#CHESSBOARD_SITE_METHOD = local
 
 CHESSBOARD_INSTALL_STAGING = YES
 
