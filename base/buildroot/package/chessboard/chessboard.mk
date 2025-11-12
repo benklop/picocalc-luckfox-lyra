1180d9962ac810311f580574efe99465a30bb5f8
Hpsaturn <hpsaturn@gmail.com>
2025-11-12 09:20:02 +0100

    fix minor: added missing dependency

--- base/buildroot/package/chessboard/chessboard.mk
+++ base/buildroot/package/chessboard/chessboard.mk
@@ -14,6 +14,6 @@ CHESSBOARD_SITE_METHOD = git
 
 CHESSBOARD_INSTALL_STAGING = YES
 
-CHESSBOARD_DEPENDENCIES = host-pkgconf extra-cmake-modules sdl2
+CHESSBOARD_DEPENDENCIES = host-pkgconf extra-cmake-modules sdl2 yaml-cpp
 
 $(eval $(cmake-package))
